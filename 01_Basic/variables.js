b173a6701655c0b6a6eb59213be992352f45a621
divyxnshh <161883812+divyxnshh@users.noreply.github.com>
2024-03-02 14:20:21 +0000

    basic01

---
+++ 01_Basic/variables.js
@@ -0,0 +1,12 @@
+const accountId = 122
+let accName = "Dev"
+var accEmail = "dev@dev.com"
+let accType
+
+console.table([accountId,accName,accEmail,accType])
+
+accName = "d_Dev"
+accEmail = "mail@mail.com"
+accType = false
+
+console.table([accountId,accName,accEmail,accType])
